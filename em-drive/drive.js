a7d120a49b13a8f95bca376aeda6ea6d57b600fd
Mateusz Borycki <mateuszborycki@gmail.com>
2017-08-10 23:10:13 +0200

    Move code from drive.js to test embedding.

--- em-drive/drive.js
+++ em-drive/drive.js
@@ -3,9 +3,6 @@ window.EmDrive = window.EmDrive || {}
 // Array of API discovery doc URLs for APIs used by the quickstart
 var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 
-var authorizeButton = document.getElementById('authorize-button');
-var signoutButton = document.getElementById('signout-button');
-
 var Promise = require('promise');
 var initializePromiseResolve;
 
@@ -33,77 +30,5 @@ function initClient() {
 		scope: EM_GAPI_SCOPES
 	}).then(function () {
 		initializePromiseResolve();
-		// Listen for sign-in state changes.
-		gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-		// Handle the initial sign-in state.
-		updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-		authorizeButton.onclick = handleAuthClick;
-		signoutButton.onclick = handleSignoutClick;
-	});
-}
-
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-function updateSigninStatus(isSignedIn) {
-	if (isSignedIn) {
-		authorizeButton.style.display = 'none';
-		signoutButton.style.display = 'block';
-		listFiles();
-	} else {
-		authorizeButton.style.display = 'block';
-		signoutButton.style.display = 'none';
-	}
-}
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick(event) {
-	gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event) {
-	gapi.auth2.getAuthInstance().signOut();
-}
-
-/**
- * Append a pre element to the body containing the given message
- * as its text node. Used to display the results of the API call.
- *
- * @param {string} message Text to be placed in pre element.
- */
-function appendPre(message) {
-	var pre = document.getElementById('content');
-	var textContent = document.createTextNode(message + '\n');
-	pre.appendChild(textContent);
-}
-
-/**
- * Print files.
- */
-function listFiles() {
-	//q
-	//pageToken
-	//spaces - appDataFolder
-	gapi.client.drive.files.list({
-		'pageSize': 10,
-		'fields': "nextPageToken, files(id, name)"
-	}).then(function(response) {
-		appendPre('Files:');
-		var files = response.result.files;
-		if (files && files.length > 0) {
-			for (var i = 0; i < files.length; i++) {
-				var file = files[i];
-				appendPre(file.name + ' (' + file.id + ')');
-			}
-		} else {
-			appendPre('No files found.');
-		}
 	});
 }
