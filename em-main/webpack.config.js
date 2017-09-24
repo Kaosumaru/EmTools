2a29e4fe136e774e51a90d0429e9ff12767d6842
Mateusz Borycki <mateuszborycki@gmail.com>
2017-09-24 23:33:27 +0200

    Add TS loader.

--- em-main/webpack.config.js
+++ em-main/webpack.config.js
@@ -3,11 +3,23 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
   output: {
     filename: 'dist.js',
     path: path.resolve(__dirname, '@CMAKE_RUNTIME_OUTPUT_DIRECTORY@/js')
   },
-  resolve: { modules: ['node_modules', path.resolve(__dirname, "@CMAKE_BINARY_DIR@/node/node_modules")] },
+  resolve: {
+      modules: ['node_modules', path.resolve(__dirname, "@CMAKE_BINARY_DIR@/node/node_modules")],
+      extensions: [ ".tsx", ".ts", ".js" ]
+  },
   //modules: [path.resolve(__dirname, "@CMAKE_BINARY_DIR@/node/node_modules"), "node_modules"],
   plugins: [
     new webpack.DefinePlugin({
